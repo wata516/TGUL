ae18b0e4c9afc8358c9962c9a70c03900b2b54c9
Koichiro <watanabe516@me.com>
2013-04-14 22:43:56 +0900

    commit

--- Sources/TGUL/String.h
+++ Sources/TGUL/String.h
@@ -7,6 +7,9 @@ namespace TGUL
     class String : public std::string
     {
     public:
+		String( ) : std::string() {}
         String( const char *str ) : std::string( str ) {}
+		
+		operator const char *() { return c_str(); }
     };
 }
